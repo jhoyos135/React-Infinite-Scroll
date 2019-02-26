2cf32528129a3243ae1b4f35c22b12c5e9713dd6
Julian Hoyos <jhoyos135@gmail.com>
2019-02-26 01:00:43 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send({
+        test: 'hello'
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT)
+console.log(`listening to port ${PORT}`);
