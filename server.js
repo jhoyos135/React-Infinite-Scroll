fc1d9e184fa5f036698135d70fe1d922097a7d17
Julian Hoyos <jhoyos135@gmail.com>
2019-02-26 11:00:25 -0500

    proxy fix

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.get('/api/photos', (req,res) => { unsplash
         });
 });
 
+if(process.env.NODE_ENV) {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT)
